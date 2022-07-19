266f739c73a5ecdc7c347844bf6376189353afc5
Prasadkpd <2019cs078@stu.ucsc.lk>
2022-07-19 20:27:47 +0530

    Completed

---
+++ MathClient.java
@@ -0,0 +1,30 @@
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RMISecurityManager;
+import java.rmi.RemoteException;
+
+public class MathClient {
+    public static void main(String[] args) {
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new RMISecurityManager());
+        }
+
+        MathService service = null;
+
+        try {
+            service = (MathService) Naming.lookup("//localhost/CalculatorService");
+
+            System.out.println("Add: " + service.add(2, 2));
+            System.out.println("Subtract: " + service.subtract(5, 2));
+            System.out.println("Multiply: " + service.multiply(2, 8));
+            System.out.println("Divide: " + service.divide(20, 2));
+        } catch (NotBoundException exception) {
+            System.err.println(exception.getMessage());
+        } catch (MalformedURLException exception) {
+            System.err.println(exception.getMessage());
+        } catch (RemoteException exception) {
+            System.err.println(exception.getMessage());
+        }
+    }
+}
